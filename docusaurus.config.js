3dd07f7f58f7304d134ddfee0592901ade16f38f
Albert Portnoy <albert@albertp.dev>
2023-01-28 21:52:28 -0600

    :moon:

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "dark",
+      },
       tableOfContents: {
         minHeadingLevel: 2,
         maxHeadingLevel: 5,
