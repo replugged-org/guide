9b0c01af98cc0c7e1baa8e82a030bb256dd07339
Fede <38290480+FedeIlLeone@users.noreply.github.com>
2023-05-20 16:39:13 +0200

    refactor: remove QuickLink

--- src/components/QuickLink.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-interface QuickLinkProps {
-  name: string;
-  url: string;
-}
-
-export default (props: QuickLinkProps): JSX.Element => <a href={props.url}>{props.name}</a>;
